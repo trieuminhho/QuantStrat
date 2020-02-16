fb02e4a6a2cf696277d8bd26be0402d82428d886
Trieu Ho <trieuho200@gmail.com>
2020-02-16 19:50:04 +0000

    now with fitting process

--- arima_strat.py
+++ arima_strat.py
@@ -1,11 +1,12 @@
 import calendar
 import datetime
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pyplot
 import numpy as np
 import pandas as pd
 import pandas_datareader as pdr
 from statsmodels.tsa.arima_model import ARIMA
 from sklearn.metrics import mean_squared_error
+
 
 def getStock(stock_name,start_date, end_date):
     """
@@ -18,6 +19,13 @@ def getStock(stock_name,start_date, end_date):
     return stock
 
 
+# Function that calls ARIMA model to fit and forecast the data
+def StartARIMAForecasting(Actual, P, D, Q):
+    model = ARIMA(Actual, order=(P, D, Q))
+    model_fit = model.fit(disp=0)
+    prediction = model_fit.forecast()[0]
+    return prediction
+
 
 if __name__ == "__main__":
 
@@ -27,61 +35,65 @@ if __name__ == "__main__":
     aapl_df = getStock("AAPL",start_date, end_date)
     aapl_log_returns = np.array(aapl_df["log_returns"])
 
-    # # forecast window length
-    # windowLength = 20
-    # forecastLength = aapl_log_returns.size - windowLength
-    #
-    # for day in range(forecastLength):
-    #
-    #     aapl_log_returns_offset = aapl_log_returns[(1+day):(windowLength+day)]
-    #
-    #     #fitting arima model
-    #     for p in range(5):
-    #         for q in range(5):
-    #
-    #             if(p==0 and q==0):
-    #                 continue
-    #             # fit model
-    #             model = ARIMA(aapl_log_returns_offset, order=(p,0,q))
-    #
-    #             model_fit = model.fit(disp=0)
-    #             print(model_fit.resid)
-    #
-    #             # plot residual errors
-    #             residuals = DataFrame(model_fit.resid)
-    #             residuals.plot()
-    #             pyplot.show()
-    #             residuals.plot(kind='kde')
-    #             pyplot.show()
-    #             print(residuals.describe())
+    NumberOfElements = len(aapl_log_returns)
+
+    # Use 70% of data as training, rest 30% to Test model
+    TrainingSize = int(NumberOfElements * 0.7)
+    TrainingData = aapl_log_returns[0:TrainingSize]
+    TestData = aapl_log_returns[TrainingSize:NumberOfElements]
 
 
-    size = int(len(aapl_log_returns) * 0.9)
-    train, test = aapl_log_returns[0:size], aapl_log_returns[size:len(aapl_log_returns)]
-    history = [aapl_log_returns for aapl_log_returns in train]
-    predictions = list()
+    min_error = 10
+    p_best = 0
+    q_best = 0
+    for p in range(5):
+        for q in range(5):
+            if(p==0 and q==0):
+                continue
 
-    print("size " + str(size))
-    print("train size" + str(len(train)))
-    print("test size " + str(len(test)))
-    print(history)
+            # new arrays to store actual and predictions
+            Actual = [x for x in TrainingData]
+            Predictions = list()
 
+            # in a for loop, predict values using ARIMA model
+            for t in range(len(TestData)):
+                ActualValue = TestData[t]
+                # forcast value
+                Prediction = StartARIMAForecasting(Actual, p, 0, q)
+                #print('Actual=%f, Predicted=%f' % (ActualValue, Prediction))
+                # add it in the list
+                Predictions.append(Prediction)
+                Actual.append(ActualValue)
 
+            # Print MSE to see how good the model is
+            Error = mean_squared_error(TestData, Predictions)
+            print("p = " + str(p))
+            print("q = " + str(q))
+            print('Test Mean Squared Error (smaller the better fit): %.6f' % Error)
+            if(Error < min_error):
+                min_error = Error
+                p_best = p
+                q_best = q
 
+    print("p_best = " + str(p_best))
+    print("q_best = " + str(q_best))
 
-    for t in range(len(test)):
-        model = ARIMA(history, order=(5, 0, 1))
-        model_fit = model.fit(disp=0)
-        output = model_fit.forecast()
-        yhat = output[0]
-        predictions.append(yhat)
-        obs = test[t]
-        history.append(obs)
-        print('predicted=%f, expected=%f' % (yhat, obs))
+    # new arrays to store actual and predictions
+    Actual = [x for x in TrainingData]
+    Predictions = list()
 
-    error = mean_squared_error(test, predictions)
-    print('Test MSE: %.3f' % error)
+    # in a for loop, predict values using ARIMA model
+    for t in range(len(TestData)):
+        ActualValue = TestData[t]
+        # forcast value
+        Prediction = StartARIMAForecasting(Actual, p_best, 0, q_best)
+        # print('Actual=%f, Predicted=%f' % (ActualValue, Prediction))
+        # add it in the list
+        Predictions.append(Prediction)
+        Actual.append(ActualValue)
     # plot
-    plt.plot(test)
-    plt.plot(predictions, color='red')
-    plt.show()
+    pyplot.plot(TestData)
+    pyplot.plot(Predictions, color='red')
+    pyplot.show()
+
+
