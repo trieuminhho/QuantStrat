529b345149ba7365e4ca47e0f33e1843d7458375
Trieu Ho <trieu.ho@flextrade.com>
2020-02-19 14:33:38 +0000

    added stls fitting functions

--- stls_strat.py
+++ stls_strat.py
@@ -22,7 +22,11 @@ def construct_candidate_matrix(candidates, state):
     candidate_matrix = np.zeros((len(state), len(candidates)))
     for i in range(len(candidates)):
         for j in range(len(state)):
-            candidate_matrix[j][i] = float(candidates[i].subs(x, state[j]))
+            if(isinstance(candidates[i], int)):
+                candidate_matrix[j][i] = float(candidates[i])
+            else:
+                candidate_matrix[j][i] = float(candidates[i].subs(x, state[j]))
+
     return candidate_matrix
 
 
@@ -41,33 +45,57 @@ def stls(theta, x_dot, threshold):
             eta[index_val] = 0
 
         big_indices_mat = np.zeros([len(big_indices), len(theta)])
-        for index in range(len(big_indices)):
-            big_indices_mat[index] = theta[big_indices[index]]
 
-        print(big_indices_mat)
+        for index in range(len(big_indices_mat)):
+            for index2 in range(len(big_indices_mat[index])):
+                big_indices_mat[index][index2] = theta[index2][big_indices[index]]
 
-        eta_big, r, rr, s = np.linalg.lstsq(big_indices_mat, x_dot, rcond=None)
-
-
+        eta_big, r, rr, s = np.linalg.lstsq(np.transpose(big_indices_mat), x_dot, rcond=None)
 
         for index in range(len(eta_big)):
             eta[big_indices[index]] = eta_big[index]
 
-        print(eta - eta_temp)
-        #error = np.linalg.norm(eta - eta_temp)
+        error = np.linalg.norm(eta - eta_temp)
 
-    return eta
+    terms = np.count_nonzero(eta)
+    return eta, terms
 
-
-
-
-
-
-
-
+def sym_power(x, n):
+    power = 1
+    for i in range(1, n):
+        power = power * x
+    return power
 
 
+def stls_optimal_threshold(theta, x_dot, thresholds):
+    term_list = []
+    for threshold in thresholds:
+        _, terms = stls(theta, x_dot, threshold)
+        term_list.append(terms)
 
+    pyplot.plot(thresholds, term_list)
+    pyplot.xlabel("Thresholds")
+    pyplot.ylabel("Number of terms")
+    pyplot.show()
+
+    return 0
+
+
+def print_equation(coefficients, functions):
+    equation = 0
+    for coeff_index in range(len(coefficients)):
+        equation += coefficients[coeff_index] * functions[coeff_index]
+
+    print(equation)
+
+    return equation
+
+
+def plot_estimation(equation, actual):
+    price_estimation = []
+    for time in range(len(actual)):
+        price_estimation.append(float(equation.subs(x, time)))
+    return price_estimation
 
 
 if __name__ == "__main__":
@@ -85,9 +113,28 @@ if __name__ == "__main__":
     # pyplot.show()
 
     x = sym.Symbol('x')
+    candidate_matrix = []
 
-    candidate_matrix = [x, x*x, x*x*x, x*x*x*x]
+    for i in range(1, 50):
+        candidate_matrix.append(sym_power(x, i))
 
     candidate_matrix_values = construct_candidate_matrix(candidate_matrix, aapl_log_returns)
 
-    print(stls(candidate_matrix_values, aapl_log_returns_dot, 0.001))
+    thresholds = []
+    for n in np.linspace(-5, 1, 10):
+        thresholds.append(np.float_power(10, n))
+
+    stls_optimal_threshold(candidate_matrix_values, aapl_log_returns_dot, thresholds)
+
+    # eta, _ = stls(candidate_matrix_values, aapl_log_returns_dot, 2.18)
+    #
+    # equation = print_equation(eta, candidate_matrix)
+    # price_estimation = plot_estimation(equation, aapl_log_returns)
+    #
+    #
+    # pyplot.plot(aapl_log_returns)
+    # pyplot.plot(price_estimation)
+    # pyplot.xlabel("Time unit")
+    # pyplot.ylabel("Prices")
+    # pyplot.show()
+    #
